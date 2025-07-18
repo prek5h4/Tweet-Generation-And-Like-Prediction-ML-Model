07bdcc2fe48eaf69152e34f4c093a14366910176
prek5h4 <cs5240987@iitd.ac.in>
2025-07-18 22:09:15 +0530

    make the changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 from textblob import TextBlob
 import numpy as np
 import pandas as pd
-import pickle , os, requests
+import os
 
 app = Flask(__name__)
 generator = SimpleTweetGenerator()
@@ -134,4 +134,5 @@ def form():
         return render_template("form.html")
     
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port, debug=False)
